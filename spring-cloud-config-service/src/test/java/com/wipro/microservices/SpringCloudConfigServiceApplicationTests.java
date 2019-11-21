d51f2aa97810f943cf11c04ed7e511703da34306
Ramu Sunkari <ramusunkari444@gmail.com>
2019-11-21 23:01:09 +0530

    first commit

---
+++ spring-cloud-config-service/src/test/java/com/wipro/microservices/SpringCloudConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
